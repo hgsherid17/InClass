31a848dd9eb7521c1d2d8e512a2b9d445f404c7a
hgsherid17 <98112100+hgsherid17@users.noreply.github.com>
2023-03-21 10:23:57 -0400

    Adding comments

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -5,7 +5,10 @@
 
 int main()
 {
+    // Store selected date and time in a string
     std::string date_str = "2022-03-17 10:45:30";
+   
+    // Format date-time string
     std::tm date_obj = {};
     std::istringstream ss(date_str);
     ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
@@ -13,6 +16,7 @@ int main()
     formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
     std::string formatted_date = formatted_date_ss.str();
 
+    // Output formatted date-time string
     std::cout << formatted_date << std::endl;
 
     return 0;
