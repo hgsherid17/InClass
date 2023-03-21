c7a69375e1c2e2b3da0cae3f45f9ae43a2f2adab
Alex Vokey <112211687+AVokey@users.noreply.github.com>
2023-03-21 10:23:22 -0400

    Update even_odd_site.cc

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -4,20 +4,27 @@ using namespace std;
 
 int main() {
     ofstream file;
+    // Open file from directory
     file.open("numbers.html");
+    // Display title block
     file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
+    // Display table
     file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
     for (int i = 1; i <= 50; i++) {
         if (i % 2 == 0) {
+            // Add even numbers to table
             file << "<tr><td>" << i << "</td><td></td></tr>\n";
         }
         else {
+            // Add odd numbers to table
             file << "<tr><td></td><td>" << i << "</td></tr>\n";
         }
     }
     file << "</table>\n</body>\n</html>";
+    // Close file
     file.close();
     ifstream input("numbers.html");
+    // Load html to buffer
     cout << input.rdbuf();
     input.close();
     return 0;
